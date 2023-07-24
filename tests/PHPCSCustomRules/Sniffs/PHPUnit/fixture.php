0fc1802f90a41e268e30e0b3796170925ca17f84
naopusyu <nd450984@gmail.com>
2023-07-25 00:37:34 +0900

    add DeprecatedTestAnnotationSniff unit test

---
+++ tests/PHPCSCustomRules/Sniffs/PHPUnit/fixture.php
@@ -0,0 +1,33 @@
+<?php
+
+class CodeTest
+{
+    /**
+     * @test aaaa
+     */
+    public function select()
+    {
+    }
+
+    /** @test */
+    public function update()
+    {
+    }
+
+    /**
+     * @test
+     * @return bool
+     */
+    public function delete()
+    {
+        return true;
+    }
+
+    /**
+     * @test
+     * aaaaaa
+     */
+    public function insert()
+    {
+    }
+}
